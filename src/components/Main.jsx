0a749e1d2969483a0f810a92093951a91209a4d1
aiskairr <aminhansss2222@gmail.com>
2022-12-04 14:35:23 +0600

    feat/ finish main layout

--- src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import css from "./main.module.scss";
-
-export default function Main() {
-  return( 
-  <div className={css.main}>
-    
-  </div>)
-}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import scss from "./main.module.scss";
+
+export default function Main() {
+  return (
+    <div className={scss.main__container}>
+      <div className={scss.img__blur}>
+        <hr className={scss.first__hr} />
+        <div className={scss.main__content}>
+          <div className={scss.site__content}>
+            <div className={scss.container}>
+              <div className={scss.header}>
+                <img src="./images/solid_logo.svg" alt="logo" />
+                <h1>Бесплатный вебинар</h1>
+              </div>
+            </div>
+            <main>
+                <h1>Как начать свой путь в IT с ранних лет</h1>
+                <h2>
+                  Развиваем интеллект вашего ребенка с помощью программирования
+                </h2>
+                <button>Записаться на вебинар</button>
+                <div className={scss.mask__group}>
+                  <img src="./images/kairat.svg" alt="mask_group" />
+                  <div className={scss.infO__content}>
+                    <span>ментор</span> <br />
+                    <span>Асылбеков Кайрат, опыт 2 года в программировании</span>
+                  </div>
+                </div>
+                <div className={scss.bonus__content}>
+                    <img src="./images/helper.svg" alt="helper" />
+                    <p>Запишитесь и получите бонус</p>
+                </div>
+              </main>
+          </div>
+        </div>
+        <hr className={scss.second__hr} />
+      </div>
+    </div>
+  );
+}
